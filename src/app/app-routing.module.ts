263d62bf87d027131497904ed72743b5e15de2b6
sebasgaspar <sebasgaspar2009@hotmail.com>
2021-01-07 00:44:29 -0500

    UpdateDesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LogginComponent } from './Pages/login/login.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { FacturacionComponent } from './Pages/facturacion/facturacion.component';
-import { ClientesComponent } from './Components/clientes/clientes/clientes.component';
 import { ClientsComponent } from './Pages/clients/clients.component';
-import { ClientTableComponent } from './Components/clientes/client-table/client-table.component';
 
 const routes: Routes = [
   { path: '', component: LogginComponent },
   {
     path: 'home', component: HomeComponent, children: [
       { path: 'facturacion', component: FacturacionComponent },
-
-    ]
-  },
-  {
-    path: 'clients', component: HomeComponent, children: [
-      { path: 'clientTable', component: ClientTableComponent },
-
+      { path: 'clients', component: ClientsComponent },
     ]
   },
 ];
