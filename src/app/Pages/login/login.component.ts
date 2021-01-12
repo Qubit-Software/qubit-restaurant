d4d606171b1abbc4a9111b5fc5319d57d481ee1b
Juan Diego Tovaria <juken.555@hotmail.com>
2021-01-12 15:38:31 -0500

    cambios a sweet alert

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-loggin',
   templateUrl: './login.component.html',
@@ -10,6 +10,29 @@ export class LogginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.login();
+  }
+  private login() {
+    Swal.fire({
+      allowOutsideClick: false,
+      title: 'Espere por favor'
+    });
+    Swal.showLoading();
+    setTimeout(() => {
+      Swal.close();
+      this.error();
+      setTimeout(() => {
+        Swal.close();
+      }, 2000);
+    }, 2000);
+
   }
 
+  private error() {
+    Swal.fire({
+      title: 'Oops... Something went wrong',
+      icon: 'error',
+      showConfirmButton: false
+    })
+  }
 }
